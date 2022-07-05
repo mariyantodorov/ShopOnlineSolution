1fcf02b63a637d50731da873d63366496300cee0
Mariyan Todorov <mariyan.todorov@landmark.co.uk>
2022-07-05 10:27:30 +0300

    api improvements

--- ShopOnline.Api/Controllers/ProductController.cs
+++ ShopOnline.Api/Controllers/ProductController.cs
@@ -23,15 +23,14 @@ namespace ShopOnline.Api.Controllers
             try
             {
                 var products = await this.productRepository.GetItems();
-                var productCategories = await this.productRepository.GetCategories();
 
-                if (products == null || productCategories == null)
+                if (products == null)
                 {
                     return NotFound();
                 }
                 else
                 {
-                    var productDtos = products.ConvertToDto(productCategories);
+                    var productDtos = products.ConvertToDto();
 
                     return Ok(productDtos);
                 }
@@ -55,9 +54,7 @@ namespace ShopOnline.Api.Controllers
                 }
                 else
                 {
-                    var productCategory = await this.productRepository.GetCategory(product.CategoryId);
-
-                    var productDto = product.ConvertToDto(productCategory);
+                    var productDto = product.ConvertToDto();
 
                     return Ok(productDto);
                 }
@@ -93,15 +90,14 @@ namespace ShopOnline.Api.Controllers
             try
             {
                 var products = await productRepository.GetItemsByCategory(categoryId);
-                var productCategories = await this.productRepository.GetCategories();
 
-                if (products == null || productCategories == null)
+                if (products == null)
                 {
                     return NotFound();
                 }
                 else
                 {
-                    var productDtos = products.ConvertToDto(productCategories);
+                    var productDtos = products.ConvertToDto();
 
                     return Ok(productDtos);
                 }
